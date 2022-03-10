a9f91f52a9767f7a2cdf1a3efd8fc4c7eca4ef25
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-03-10 21:49:13 +0200

    3rd

--- Mine/app.js
+++ Mine/app.js
@@ -6,6 +6,15 @@ const fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=50&offset=0") //Getting data from url
     .then((res) => res.json()) // connecting to server, browser connecting to server
     .then((data) => {
+      // .then((data) => {
+      //   console.log(data);
+      //   array.push(data.results);
+      // }); (1.)
+      /////
+      // .then((data) => {
+      //   console.log("fetch result", data); //data is response from server
+      //   pokeCards(data.results); //passing the data triggering the next fuctuion
+      // });
       const fetches = data.results.map((p) => {
         return fetch(p.url).then((res) => res.json());
       });
